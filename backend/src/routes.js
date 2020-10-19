7c76db213e7b5e4cc6430e53fd901e154721bd84
Patrick Deitos Cremonese <patrick.deitoscremonese@gmail.com>
2020-10-19 16:35:04 -0300

    master

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi} = require('celebrate');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const SessionController = require('./controllers/SessionController');
@@ -21,7 +22,15 @@ routes.get('/profile', ProfileController.index);
 
 routes.get('/ongs', OngController.index);
 routes.get('/ong/:id', OngController.find);
-routes.post('/ongs', OngController.create);
+routes.post('/ongs', celebrate({
+    [Segments.BODY] : Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email,
+        whatsapp: Joi.number().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}), OngController.create);
 
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
